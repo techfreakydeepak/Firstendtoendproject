be47eb2bc38d2ab03ddac038707f7092f1563417
techfreakydeepak <dpsingh1518@gmail.com>
2024-07-16 20:29:39 +0530

    prediction pipeline uploaded

---
+++ App.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import sys
+from src.GemstonePricePrediction.exception import CustomException
+from src.GemstonePricePrediction.logger import logging
+from src.GemstonePricePrediction.utils.utils import load_object
+
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+    
+        
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-import os
+'''import os
 path ="notebook/research.ipynb"
 
 dir,file=os.path.split(path)
 os.makedirs(dir,exist_ok=True)
 
 with open(path,"w")as f:
-    pass
+    pass'''
+
+# test.py
+
+from src.GemstonePricePrediction.pipelines.prediction_pipeline import CustomData
+
+custdataobj = CustomData(0.33, "Premium", "G", "IF", 60.8, 58, 4.42, 4.46, 2.7)
+data = custdataobj.get_data_as_dataframe()
+
+print(data)
