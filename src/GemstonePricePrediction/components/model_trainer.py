e16fcb2a31c725d68824ab3d04312d63d4ff7d38
techfreakydeepak <dpsingh1518@gmail.com>
2024-07-16 19:31:08 +0530

    code updated

--- src/GemstonePricePrediction/components/model_trainer.py
+++ src/GemstonePricePrediction/components/model_trainer.py
@@ -1,44 +1,41 @@
 import pandas as pd
 import numpy as np
 from src.GemstonePricePrediction.logger import logging
-from src.GemstonePricePrediction.exception import customexception
+from src.GemstonePricePrediction.exception import CustomException
 import os
 import sys
 from dataclasses import dataclass
 from pathlib import Path
 
-from src.GemstonePricePrediction.utils import save_object,evaluate_model
+from src.GemstonePricePrediction.utils import save_object, evaluate_model
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 
-from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
-
-
-@dataclass 
+@dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
-    
-    
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
     
-    def initate_model_training(self,train_array,test_array):
+    def initiate_model_training(self, train_array, test_array):
         try:
             logging.info('Splitting Dependent and Independent variables from train and test data')
             X_train, y_train, X_test, y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
             )
 
-            models={
-            'LinearRegression':LinearRegression(),
-            'Lasso':Lasso(),
-            'Ridge':Ridge(),
-            'Elasticnet':ElasticNet()
-        }
+            models = {
+                'LinearRegression': LinearRegression(),
+                'Lasso': Lasso(),
+                'Ridge': Ridge(),
+                'Elasticnet': ElasticNet()
+            }
             
-            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            model_report = evaluate_model(X_train, y_train, X_test, y_test, models)
             print(model_report)
             print('\n====================================================================================\n')
             logging.info(f'Model Report : {model_report}')
@@ -52,19 +49,15 @@ class ModelTrainer:
             
             best_model = models[best_model_name]
 
-            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print(f'Best Model Found, Model Name: {best_model_name}, R2 Score: {best_model_score}')
             print('\n====================================================================================\n')
-            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            logging.info(f'Best Model Found, Model Name: {best_model_name}, R2 Score: {best_model_score}')
 
             save_object(
                  file_path=self.model_trainer_config.trained_model_file_path,
                  obj=best_model
             )
-          
 
         except Exception as e:
-            logging.info('Exception occured at Model Training')
-            raise customexception(e,sys)
-
-        
-    
+            logging.info('Exception occurred at Model Training')
+            raise CustomException(e, sys)
--- src/GemstonePricePrediction/pipelines/training_pipeline.py
+++ src/GemstonePricePrediction/pipelines/training_pipeline.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import Path
 from src.GemstonePricePrediction.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.GemstonePricePrediction.components.data_transformation import DataTransformation
+from src.GemstonePricePrediction.components.model_trainer import ModelTrainer
+from src.GemstonePricePrediction.exception import CustomException
 
 def main():
     try:
@@ -15,8 +17,9 @@ def main():
         data_transformation = DataTransformation()
         train_arr, test_arr = data_transformation.initialize_data_transformation(data_ingestion_config.train_data_path, data_ingestion_config.test_data_path)
 
-        # Further processing (model training, evaluation, etc.) can be added here
-        # ...
+        # Model Training
+        model_trainer = ModelTrainer()
+        model_trainer.initiate_model_training(train_arr, test_arr)
 
     except Exception as e:
         raise CustomException(e, sys)
